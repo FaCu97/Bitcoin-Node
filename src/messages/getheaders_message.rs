71078322c0c0c311df70afe016cde217cf59270a
martinbucca <78556820+martinbucca@users.noreply.github.com>
2023-05-06 22:09:38 -0300

    write_to de GetHeadersMessage

--- src/messages/getheaders_message.rs
+++ src/messages/getheaders_message.rs
@@ -11,7 +11,7 @@ pub struct GetHeadersMessage {
 impl GetHeadersMessage {
     pub fn write_to(&self, stream: &mut dyn Write) -> std::io::Result<()> {
         let header = self.header.to_le_bytes();
-        let payload = self.payload.to_le_bytes();
+        let payload: Vec<u8> = self.payload.to_le_bytes();
         let mut message: Vec<u8> = Vec::new();
         message.extend_from_slice(&header);
         message.extend(payload);
