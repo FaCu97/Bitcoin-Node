cc1258eadf0e5bcd422caa432286b5f9a0744fab
Facundo de la Plata <fdelaplata@fi.uba.ar>
2023-05-12 00:49:24 -0300

    Refactor conexion DNS

--- src/network.rs
+++ src/network.rs
@@ -1,22 +1,31 @@
-use std::process::Command;
+use std::net::{ToSocketAddrs, SocketAddr, Ipv4Addr};
+
+use crate::config::{Config};
 const NUMBER_OF_NODES: usize = 8;
 
-pub fn get_active_nodes_from_dns_seed(dns_seed: String) -> std::io::Result<Vec<String>> {
-    let query_reply = Command::new("dig").arg("+short").arg(dns_seed).output()?;
-    let active_nodes = String::from_utf8_lossy(&query_reply.stdout);
-    let mut nodes_list = vec![];
-    for node in active_nodes.lines().take(NUMBER_OF_NODES) {
-        nodes_list.push(node.to_string());
-    }
-    if nodes_list.len() < NUMBER_OF_NODES {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "No se obtuvieron la cantidad necesaria de nodos de la DNS seed! \n",
-        ));
-    }
-    Ok(nodes_list)
-}
+pub fn get_active_nodes_from_dns_seed(config: &Config) -> std::io::Result<Vec<Ipv4Addr>> {
+    let mut node_ips= Vec::new();
+    let host = "seed.testnet.bitcoin.sprovoost.nl";
+    let port = 8333;
 
+    let addrs = match (host, port).to_socket_addrs() {
+        Ok(addrs) => addrs,
+        Err(e) => {
+         //   println!("Error al resolver {}: {}", host, e);
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "No se pudo conectar a la DNS seed!\n",
+            ));
+        }
+    };
+    for addr in addrs {
+        if let SocketAddr::V4(v4_addr) = addr {
+            node_ips.push(*v4_addr.ip());
+        }
+    }
+    Ok(node_ips)
+}
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -67,3 +76,4 @@ mod tests {
         )
     }
 }
+*/
