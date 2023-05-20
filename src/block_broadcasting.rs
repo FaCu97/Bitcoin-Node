db2c75019f7d0a900f7966db336a1845824e8902
martinbucca <78556820+martinbucca@users.noreply.github.com>
2023-05-20 16:38:55 -0300

    cambio para que se muestre ip en log y no tcpstream

--- src/block_broadcasting.rs
+++ src/block_broadcasting.rs
@@ -1,5 +1,5 @@
 use std::{net::TcpStream, sync::{Arc, RwLock}, thread::{JoinHandle, self}, io::Read};
-use crate::{block_header::BlockHeader, block::Block, messages::message_header::{HeaderMessage, command_name_to_bytes}, log_writer::{LogSender, write_in_log}, node};
+use crate::{block_header::BlockHeader, block::Block, messages::{message_header::{HeaderMessage, command_name_to_bytes}, headers_message::HeadersMessage}, log_writer::{LogSender, write_in_log}, node};
 
 
 const HEADERS: &str = "headers\0\0\0\0\0";
@@ -27,18 +27,8 @@ pub fn listen_for_incoming_blocks_from_node(log_sender: LogSender, mut node: Tcp
     let t = thread::spawn(move || {
         loop {
             println!("Estoy esperando leer algo\n");
-            let mut buffer_num = [0; 24];
-            node.read_exact(&mut buffer_num).unwrap();
-            let mut header = HeaderMessage::from_le_bytes(buffer_num).unwrap();
-            let payload_size = header.payload_size as usize;
-            let mut payload_buffer_num: Vec<u8> = vec![0; payload_size];
-            node.read_exact(&mut payload_buffer_num).unwrap();
-            println!("Lei algooo!!!!\n");
-            let h = HEADERS.to_string();
-            match header.command_name {
-                h => write_in_log(log_sender_clone.info_log_sender.clone(), "ENCONTRE UN HEADERS!!!!"),
-                _ => println!("Me llego otra cosa que no es un headers!"),
-            }
+            let header = HeadersMessage::read_from(log_sender_clone.clone(), &mut node).unwrap();
+            // pedir bloques
         }
 
     });
--- src/initial_block_download.rs
+++ src/initial_block_download.rs
@@ -471,7 +471,7 @@ fn request_blocks_from_node(
     match GetDataMessage::new(inventory).write_to(&mut node) {
         Ok(_) => Ok(()),
         Err(err) => {
-            write_in_log(log_sender.error_log_sender,format!("Error: No puedo pedir {:?} cantidad de bloques del nodo: {:?}. Se los voy a pedir a otro nodo", chunk_llamada.len(), node).as_str());
+            write_in_log(log_sender.error_log_sender,format!("Error: No puedo pedir {:?} cantidad de bloques del nodo: {:?}. Se los voy a pedir a otro nodo", chunk_llamada.len(), node.peer_addr()).as_str());
 
             tx.send(block_headers_thread.to_vec())
                 .map_err(|err| DownloadError::ThreadChannelError(err.to_string()))?;
@@ -500,7 +500,7 @@ fn receive_requested_blocks_from_node(
         let bloque = match BlockMessage::read_from(log_sender.clone(), &mut node) {
             Ok(bloque) => bloque,
             Err(err) => {
-                write_in_log(log_sender.error_log_sender,format!("No puedo descargar {:?} de bloques del nodo: {:?}. Se los voy a pedir a otro nodo y descarto este. Error: {err}", chunk_llamada.len(), node).as_str());
+                write_in_log(log_sender.error_log_sender,format!("No puedo descargar {:?} de bloques del nodo: {:?}. Se los voy a pedir a otro nodo y descarto este. Error: {err}", chunk_llamada.len(), node.peer_addr()).as_str());
                 tx.send(block_headers_thread.to_vec())
                     .map_err(|err| DownloadError::ThreadChannelError(err.to_string()))?;
                 // falló la recepción del mensaje, tengo que intentar con otro nodo
