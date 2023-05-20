14a9782318f8dade3266b296a9f07fea322a2189
nicolastonizzo1 <ntonizzo@fi.uba.ar>
2023-05-20 14:49:59 -0300

    debugeo de proof of work

--- src/block_header.rs
+++ src/block_header.rs
@@ -81,12 +81,22 @@ impl BlockHeader {
         let nonce_bytes = self.nonce.to_le_bytes();
         marshaled_block_header.extend_from_slice(&nonce_bytes);
     }
+    fn reverse_bytes(bytes: &[u8]) -> Vec<u8> {
+        let mut reversed_bytes = bytes.to_vec();
+        reversed_bytes.reverse();
+        reversed_bytes
+    }
     pub fn hash(&self) -> [u8; 32] {
         let mut block_header_marshaled: Vec<u8> = Vec::new();
         self.marshalling(&mut block_header_marshaled);
         let hash_block = sha256d::Hash::hash(&block_header_marshaled);
-        *hash_block.as_byte_array()
+        let hash_be = *hash_block.as_byte_array();
+        let hash_le = Self::reverse_bytes(&hash_be);
+        let mut hash:[u8;32] = [0;32];
+        hash.copy_from_slice(&hash_le);
+        hash
     }
+
     // Esta funcion realiza la proof of work
     pub fn validate(&self) -> bool {
         let n_bits_bytes = self.n_bits.to_be_bytes();
@@ -310,8 +320,12 @@ mod tests {
             block_header_message_expected[x] = x as u8;
         }
         let expected_hash = sha256d::Hash::hash(&block_header_message_expected);
+        let expected_hash_be =  *expected_hash.as_byte_array();
+        let expected_hash_le = BlockHeader::reverse_bytes(&expected_hash_be);
+        let mut hash_expected:[u8;32] = [0;32];
+        hash_expected.copy_from_slice(&expected_hash_le);
         let hash: [u8; 32] = block_header.hash();
-        assert_eq!(hash, *expected_hash.as_byte_array())
+        assert_eq!(hash,hash_expected )
     }
 
     #[test]
