ac42f618ef2a5e2aed2a77977b2a57eeed6bcaba
martinbucca <78556820+martinbucca@users.noreply.github.com>
2023-06-06 14:49:39 -0300

    borro modulo inv_message.rs

--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -2,7 +2,6 @@ pub mod block_message;
 pub mod get_data_message;
 pub mod getheaders_message;
 pub mod headers_message;
-pub mod inv_message;
 pub mod inventory;
 pub mod message_header;
 pub mod payload;
