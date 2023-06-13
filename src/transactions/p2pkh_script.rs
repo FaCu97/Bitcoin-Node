665f76008f002a8cdbe914bd345c1189b28331aa
Facundo de la Plata <fdelaplata@fi.uba.ar>
2023-06-13 20:28:53 -0300

    cargo clippy

--- src/transactions/p2pkh_script.rs
+++ src/transactions/p2pkh_script.rs
@@ -20,12 +20,12 @@ pub fn validate(p2pkh_script: &[u8], sig_script: &[u8]) -> bool {
     sig_script_pubkey.copy_from_slice(&sig_script[72..105]);
 
     // 1) Chequeo que el primer comando sea OP_DUP (0x76)
-    if (&p2pkh_script[0..1] != [0x76]) {
+    if p2pkh_script[0..1] != [0x76] {
         return false;
     }
 
     // 2) Chequeo que el siguiente comando sea OP_HASH_160 (0xA9)
-    if (&p2pkh_script[1..2] != [0xA9]) {
+    if p2pkh_script[1..2] != [0xA9] {
         return false;
     }
 
@@ -33,21 +33,21 @@ pub fn validate(p2pkh_script: &[u8], sig_script: &[u8]) -> bool {
     let sha256_hash = Sha256::digest(sig_script_pubkey);
     let ripemd160_hash = *ripemd160::Hash::hash(&sha256_hash).as_byte_array();
     // 4) Chequeo que el siguiente comando sea OP_EQUALVERIFY (0x88)
-    if (&p2pkh_script[23..24] != [0x88]) {
+    if p2pkh_script[23..24] != [0x88] {
         return false;
     }
 
     // 5) Chequeo que los hash coincidan
-    if (&p2pkh_script[3..23] != ripemd160_hash) {
+    if p2pkh_script[3..23] != ripemd160_hash {
         return false;
     }
 
     // 6) Chequeo que el siguiente comando sea OP_CHECKSIG (0xAC)
-    if (&p2pkh_script[24..25] != [0xAC]) {
+    if p2pkh_script[24..25] != [0xAC] {
         return false;
     }
 
-    return true;
+    true
 }
 
 #[cfg(test)]
