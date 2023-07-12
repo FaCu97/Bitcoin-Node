1e82970b5716ab49acb87bcc5ea5328ea21ed799
Facundo de la Plata <fdelaplata@fi.uba.ar>
2023-07-12 00:38:35 -0300

    implemento mensaje notfound

---
+++ src/messages/notfound_message.rs
@@ -0,0 +1,13 @@
+use super::{
+    inventory::{inv_mershalling, Inventory},
+    message_header::HeaderMessage,
+};
+
+pub fn get_notfound_message(inventories: Vec<Inventory>) -> Vec<u8> {
+    let mut message = vec![];
+    let payload = inv_mershalling(inventories);
+    let header = HeaderMessage::new("notfound".to_string(), Some(&payload));
+    message.extend_from_slice(&header.to_le_bytes());
+    message.extend_from_slice(&payload);
+    message
+}
