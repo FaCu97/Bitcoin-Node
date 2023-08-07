7ce3e0f5b925aa01f176c83e3e08bb92e6625e81
Facundo de la Plata <facu_@live.com>
2023-08-07 16:02:44 -0300

    se setea el icono a las dos ventanas

--- src/gtk/ui_gtk.rs
+++ src/gtk/ui_gtk.rs
@@ -1,12 +1,12 @@
 use std::sync::mpsc::Sender;
 
 use super::ui_events::UIEvent;
+use super::ui_functions::set_icon;
 use super::{
     callbacks::connect_ui_callbacks,
     ui_functions::{add_css_to_screen, handle_ui_event},
 };
 use crate::wallet_event::WalletEvent;
-use gtk::gdk_pixbuf::{self, Pixbuf};
 use gtk::{
     glib::{self, Priority},
     prelude::*,
@@ -45,20 +45,7 @@ fn build_ui(ui_sender: &Sender<glib::Sender<UIEvent>>, sender_to_node: &Sender<W
         .object("initial-window")
         .expect("no se pudo cargar la ventana inicial");
     initial_window.set_title("Bitcoin Wallet");
-    // Cargar el icono
-    match Pixbuf::from_file("src/gtk/resources/icon.png") {
-        Ok(icon_pixbuf) => {
-            println!("OK");
-            match icon_pixbuf.scale_simple(64, 64, gdk_pixbuf::InterpType::Bilinear) {
-                Some(icon) => {
-                    println!("OK");
-                    initial_window.set_icon(Some(&icon));
-                }
-                None => println!("Error en icon"),
-            }
-        }
-        Err(e) => println!("Error en pixbuf {}", e),
-    }
+    set_icon(&initial_window);
     initial_window.show();
 
     let tx_to_node = sender_to_node.clone();
